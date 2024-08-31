1db110244db59a48a3d99aa288db9d81a363ff6d
Sakshi Umesh Chavan <142425493+SakshiS4chavan@users.noreply.github.com>
2024-08-31 16:15:01 +0530

    Add files via upload

---
+++ ERole.java
@@ -0,0 +1,10 @@
+package model;
+
+package rs.ac.singidunum.novisad.isa.model;
+
+public enum ERole {
+	ROLE_USER,
+    ROLE_STUDENT,
+    ROLE_PROFESSOR,
+    ROLE_ADMINISTRATOR
+}
---
+++ SubjectFollowing.java
@@ -0,0 +1,20 @@
+package model;
+
+package rs.ac.singidunum.novisad.isa.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "subfollowing")
+public class SubjectFollowing {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	
+}
